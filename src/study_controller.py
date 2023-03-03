622dd55b6cf91d25a14904f2907cc111d32a7a59
Brennan <brennanmk2200@gmail.com>
2023-03-03 13:22:34 -0500

    Added web app

--- src/study_controller.py
+++ src/study_controller.py
@@ -1,8 +1,9 @@
 import rospy
 import random
 from database import dbController
-from std_msgs.msg import Int8MultiArray, Bool
+from std_msgs.msg import Int8MultiArray, Int16
 import time
+
 
 class study_controller:
     def __init__(self):
@@ -14,30 +15,36 @@ class study_controller:
         self.grasp2 = 0
 
         rospy.init_node('web_ros')
-        rospy.Subscriber('next_user', Bool, self.next_user)
+        rospy.Subscriber('user', Int16, self.next_user)
 
         self.pub_grasp = rospy.Publisher('next_grasp', Int8MultiArray)
+        self.view = rospy.Publisher('next_grasp', Int8MultiArray)
 
-        time.sleep(0.1) #add short delay
+        time.sleep(0.1)  # add short delay
 
         self.next_grasp
 
         rospy.spin()
 
-    def next_user(self, data): #Iterate UID to represent a new paticipant 
-        self.uid += 1
+    def next_user(self, data):  # Iterate UID to represent a new paticipant
+        self.uid = data.data
         self.next_grasp()
 
     def next_grasp(self):
-        self.grasp1 = random.randint(0, self.num_of_grasps) 
-        self.grasp2 = random.randint(0, self.num_of_grasps) 
+        self.grasp1 = random.randint(0, self.num_of_grasps)
+        self.grasp2 = random.randint(0, self.num_of_grasps)
 
-        self.pub_grasp.publish([self.grasp1, self.grasp2])
-
-        if self.grasp2 == self.grasp1: #Verify random numbers are not the same, if they are resample
+        if self.grasp2 == self.grasp1:  # Verify random numbers are not the same, if they are resample
             self.next_grasp()
         else:
             pass
 
-    def add_entry(self, pref): #add entry db table
+    def add_entry(self, pref):  # add entry db table
         self.db.addEntry(self.uid, self.grasp1, self.grasp2, pref)
+        self.next_grasp()
+
+    def disp_grasp(self, idx):
+        if idx == 1:
+            self.view.publish(self.grasp1)
+        else if idx == 2:
+            self.view.publish(self.grasp2)
--- src/webapp.py
+++ src/webapp.py
@@ -1,26 +1,32 @@
 from flask import Flask, render_template, request
-import database
 from study_controller import study_controller
 app = Flask(__name__)
 
-controller = database.dbController()
+controller = study_controller.study_controller()
 
-study_controller = study_controller()
 
 @app.route('/', methods = ['POST', 'GET'])
 def robots():
     if request.method == 'POST':
-        if 'delete' in request.form:
-            controller.delete(request.form["delete"], 'robots')
-        elif 'update' in request.form:
-            controller.update_robot_table(request.form["update"], request.form["name"], request.form["ip"], request.form["port"], request.form["image"])
-        elif 'create' in request.form:
-            controller.add_robot(request.form["name"], request.form["ip"], request.form["port"], request.form["image"], request.form["image_target_width"])
-    return render_template("robots.html")
-
-@app.route('/robot_data')
-def robot_data():
-    return controller.getData('robots')
+        if 'view_one' in request.form:
+            controller.addEntry(1)
+        elif 'view_two' in request.form:
+            controller.addEntry(2)
+        elif 'view_three' in request.form:
+            controller.addEntry(3)
+        elif 'view_four' in request.form:
+            controller.addEntry(4)
+        elif 'view_five' in request.form:
+            controller.addEntry(5)
+        elif 'view_six' in request.form:
+            controller.addEntry(6)
+        elif 'view_seven' in request.form:
+            controller.addEntry(7)
+        elif 'view_one' in request.form:
+            controller.disp_grasp(1)
+        elif 'view_two' in request.form:
+            controller.disp_grasp(2)
+    return render_template("index.html")
     
 if __name__ == '__main__':
     app.run()
